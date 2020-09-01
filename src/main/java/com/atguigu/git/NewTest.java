4892674b866259249bc02c5521a3affcbc80f1ee
lyc <lyc@atguigu.com>
2020-09-01 18:37:09 +0800

    master test2

--- src/main/java/com/atguigu/git/NewTest.java
+++ src/main/java/com/atguigu/git/NewTest.java
@@ -4,6 +4,7 @@ public class NewTest {
     public static void main(String[] args) {
         System.out.println("hello world");//github test
         System.out.println("master");//master test
+        System.out.println("master");//master test
         System.out.println("master");//hotfix
     }
 }
