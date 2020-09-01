83c029375de6d421ab1a16be4c141146bfbec8cb
l903925039 <70572590+l903925039@users.noreply.github.com>
2020-09-01 18:31:59 +0800

    Update NewTest.java

--- src/main/java/com/atguigu/git/NewTest.java
+++ src/main/java/com/atguigu/git/NewTest.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class NewTest {
     public static void main(String[] args) {
-        System.out.println("hello world");//github
+        System.out.println("hello world");//github test
         System.out.println("master");//master
         System.out.println("master");//hotfix
     }
