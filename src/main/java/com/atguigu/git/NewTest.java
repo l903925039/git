fb91a73ddc1dbb2c492f1aca4f9425a2b69d84cc
lyc <lyc@atguigu.com>
2020-09-01 18:26:26 +0800

    master

--- src/main/java/com/atguigu/git/NewTest.java
+++ src/main/java/com/atguigu/git/NewTest.java
@@ -3,6 +3,6 @@ package com.atguigu.git;
 public class NewTest {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("master");
+        System.out.println("master");//master
     }
 }
