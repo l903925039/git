8fc8dd692dc386f78692f60b3e66766bb24c67c8
l903925039 <70572590+l903925039@users.noreply.github.com>
2020-09-01 17:09:13 +0800

    Update NewTest.java

--- src/main/java/com/atguigu/git/NewTest.java
+++ src/main/java/com/atguigu/git/NewTest.java
@@ -4,5 +4,6 @@ public class NewTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("master");
+        System.out.println("github");
     }
 }
