93c39e42cb95266fdc259ef8dfea41cce3478de9
lyc <lyc@atguigu.com>
2020-09-01 15:28:32 +0800

    my second git

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first git");
+        System.out.println("second git");
+        System.out.println("second git");
     }
 }
